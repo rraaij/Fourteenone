2707ce4b68794db1625f488baed65af0c9eaaec5
Ramon van Raaij <ramon.van.raaij@trivento.nl>
2012-12-07 10:42:59 +0100

    initial commit

---
+++ src/com/rvr/fourteenone/GameActivity.java
@@ -0,0 +1,15 @@
+package com.rvr.fourteenone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.game);
+	}
+
+}
---
+++ src/com/rvr/fourteenone/MainActivity.java
@@ -0,0 +1,28 @@
+package com.rvr.fourteenone;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void onClickStartGame(View view) {
+		Toast.makeText(getApplicationContext(), "Knop geklikt: "+view.getId(), Toast.LENGTH_LONG).show();
+	}
+
+}
